c856ae829cc2cdb5bd827558725c36476cee0294
sxannyy <sxannyy@mail.ru>
2024-10-10 09:48:21 +0800

    changed some values

--- gradient/main.py
+++ gradient/main.py
@@ -6,19 +6,19 @@ def lerp(v0, v1, t):
 
 size = 100
 image = np.zeros((size, size, 3), dtype="uint8")
+print(image)
 assert image.shape[0] == image.shape[1]
 
 color1 = [189, 238, 252]
 color2 = [241, 141, 242]
 
-for i in range(size):
-    for j in range(size):
-        right_pos = (i + j) / (2 * (size - 1))
+for y in range(size):
+    for x in range(size):
+        right_pos = (y + x) / (2 * (size - 1))
         r = int(lerp(color1[0], color2[0], right_pos))
         g = int(lerp(color1[1], color2[1], right_pos))
         b = int(lerp(color1[2], color2[2], right_pos))
-        # print(r, g, b)
-        image[i, j, :] = [r, g, b]
+        image[y, x, :] = [r, g, b]
 
 plt.figure(1)
 plt.imshow(image)
