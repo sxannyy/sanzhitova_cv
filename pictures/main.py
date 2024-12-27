27830505172d4396338d491530dcf6ee69583f60
sanya <117066798+sxannyy@users.noreply.github.com>
2024-12-27 16:24:20 +0800

    Update main.py

--- pictures/main.py
+++ pictures/main.py
@@ -9,10 +9,7 @@ image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
 cap = cv2.VideoCapture(video_path)
 
-h, w = image.shape[:2]
-
 frame_count = 0
-image_count = 0
 
 while True:
     ret, frame = cap.read()
@@ -26,14 +23,7 @@ while True:
     loc = np.where(result >= threshold)
 
     if len(loc[0]) > 800 and len(loc[0]) < 1000:
-        image_count += 1
-
-        for pt in zip(*loc[::-1]):
-            cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 2)
-            cv2.putText(frame, f'Match: {len(loc[0])}', (10, 30),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-            
-    frame_count += 1
+        frame_count += 1
 
 cap.release()
 
